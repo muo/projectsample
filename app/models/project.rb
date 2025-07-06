d4af6d01ff283dc8371e7cb84bbbb735eca84da7
muo <muo.code@seznam.cz>
2025-07-06 18:34:55 +0200

    Add has many relation to the project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :state, inclusion: { in: %w[draft open completed closed] }
+
+  has_many :comments, dependent: :destroy
 end
