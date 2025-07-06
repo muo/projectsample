907ed5c41e33f0a66296ebcb73f95fe8e4aa1c0a
muo <muo.code@seznam.cz>
2025-07-06 17:21:36 +0200

    Add select box for project state.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :state, inclusion: { in: %w[draft open completed closed] }
 end
