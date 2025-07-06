d51275f58df7963a425dd27db5e2409d08323c66
muo <muo.code@seznam.cz>
2025-07-06 17:16:26 +0200

    Change root page to projects.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  root 'home#index'
+  root 'projects#index'
   # Defines the root path route ("/")
   # root "posts#index"
 end
