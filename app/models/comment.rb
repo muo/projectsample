bf185b1d6a8925b68d0ccd28b0be6f88e98ac793
muo <muo.code@seznam.cz>
2025-07-15 09:03:41 +0200

    Add timeline create callback.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,15 @@ class Comment < ApplicationRecord
   belongs_to :project
 
   before_save :set_user_email
+  after_create :create_timeline_item
 
   private
 
   def set_user_email
     self.user_email = user.email if user.present?
+  end
+
+  def create_timeline_item
+    TimelineItem.create!(project: project, resource: self)
   end
 end
