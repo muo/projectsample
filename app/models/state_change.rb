0117064b729c896688b5f68f1ced29bf7b3742c0
muo <muo.code@seznam.cz>
2025-07-15 09:25:42 +0200

    Add timeline to project detail page.

--- app/models/state_change.rb
+++ app/models/state_change.rb
@@ -1,4 +1,12 @@
 class StateChange < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  after_create :create_timeline_item
+
+  private
+
+  def create_timeline_item
+    TimelineItem.create!(project: project, resource: self)
+  end
 end
